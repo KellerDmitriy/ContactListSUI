fd3285c84c9f3f12518e57c8040334a48131117a
DmKeller <dmit.keller@gmail.com>
2023-06-23 21:49:01 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Келлер Дмитрий on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
